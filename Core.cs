b8f3fce3a499d7f65d4450b7a82e1658b5eff39b
Tproplay <tproplayvns2208@gmail.com>
2026-04-09 09:47:26 +0530

    Added Hyno zombie and items spawning

--- Core.cs
+++ Core.cs
@@ -1,7 +1,8 @@
-﻿using MelonLoader;
+using MelonLoader;
 using UnityEngine;
 using HarmonyLib;
 using Il2Cpp;
+
 
 [assembly: MelonInfo(typeof(SimpleSpawner.Core), "Simple Spawner", "3.5", "Tproplay")]
 [assembly: MelonGame("LanPiaoPiao", "PlantsVsZombiesRH")]
@@ -17,6 +18,10 @@ namespace SimpleSpawner
 
         public override void OnUpdate()
         {
+
+            // --- Input handling for spawning plants and zombies ---
+
+            // -- Press 'P' to spawn a plant at the mouse position if a plant type is selected ---
             if (UnityEngine.Input.GetKeyDown(KeyCode.P))
             {
                 if (plantTypeselected != PlantType.Nothing)
@@ -26,18 +31,33 @@ namespace SimpleSpawner
                 }
             }
 
-            if (UnityEngine.Input.GetKeyDown(KeyCode.O))
+            // -- Press 'Left Control + O' to spawn a zombie with mind control at the mouse position if a zombie type is selected ---
+            else if (UnityEngine.Input.GetKey(KeyCode.RightControl) && UnityEngine.Input.GetKeyDown(KeyCode.O))
+            {
+                if (zombieTypeselected != ZombieType.Nothing)
+                {
+                    CreateZombie.Instance.SetZombieWithMindControl(Mouse.Instance.theMouseRow,
+                         zombieTypeselected, Mouse.Instance.mouseX);
+                }
+            }
+
+
+            // -- Press 'O' to spawn a zombie at the mouse position if a zombie type is selected ---
+            else if (!UnityEngine.Input.GetKey(KeyCode.RightControl) && UnityEngine.Input.GetKeyDown(KeyCode.O))
             {
                 if (zombieTypeselected != ZombieType.Nothing)
                 {
                     CreateZombie.Instance.SetZombie(Mouse.Instance.theMouseRow,
-                         zombieTypeselected,Mouse.Instance.mouseX);
+                         zombieTypeselected, Mouse.Instance.mouseX);
                 }
             }
 
-            if (UnityEngine.Input.GetKeyDown(KeyCode.I))
+
+
+            // -- Press 'I' to toggle the game's time scale between paused and normal ---
+            else if (UnityEngine.Input.GetKeyDown(KeyCode.I))
             {
-                if(UnityEngine.Time.timeScale != 0)
+                if (UnityEngine.Time.timeScale != 0)
                 {
                     UnityEngine.Time.timeScale = 0;
                 }
@@ -47,13 +67,49 @@ namespace SimpleSpawner
                     UnityEngine.Time.timeScale = 1;
                 }
             }
+
+            // -- Press number keys 1-9 to spawn specific items at the mouse position ---
+            for (int i = 0; i < 9; i++)
+            {
+                if (Input.GetKeyDown(KeyCode.Keypad1 + i))
+                {
+                    SpawnItem(i);
+                }
+            }
+
+
         }
 
-       
+        // Method to spawn an item based on the provided index, using the paths defined in the code
+        public void SpawnItem(int index)
+        {
+            string[] paths = new string[] {
+                "Items/Fertilize/Ferilize",
+                "Items/Bucket",
+                "Items/Helmet",
+                "Items/Jackbox",
+                "Items/Pickaxe",
+                "Items/Machine",
+                "Items/SuperMachine",
+                "Items/PortalHeart",
+                "Items/SproutPotPrize/SproutPotPrize",
+            };
 
+            if (index >= 0 && index < paths.Length)
+            {
+                GameObject prefab = Resources.Load<GameObject>(paths[index]);
+                GameObject item = UnityEngine.Object.Instantiate(prefab);
+                item.transform.position = new Vector3(Mouse.Instance.mouseX, Mouse.Instance.mouseY, 0);
+                item.transform.SetParent(GameAPP.board.transform);
+            }
+            
+        }
+
+
+        // --- Harmony patches to update the selected plant and zombie types when clicking
+        // on Almanac cards or selecting them in the Almanac menus ---
 
         [HarmonyPatch(typeof(AlmanacCard), "OnMouseDown")]
-
         public class AlmanacCard_OnMouseDown_Patch
         {
             public static void Postfix(AlmanacCard __instance)
@@ -63,9 +119,7 @@ namespace SimpleSpawner
 
         }
 
-
         [HarmonyPatch(typeof(AlmanacCardZombie), "OnMouseDown")]
-
         public class AlmanacCardZombie_OnMouseDown_Patch
         {
             public static void Postfix(AlmanacCardZombie __instance)
@@ -87,7 +141,6 @@ namespace SimpleSpawner
 
         }
 
-
         [HarmonyPatch(typeof(AlmanacZombieMenu), nameof(AlmanacZombieMenu.SelectCard))]
         public static class AlmanacZombieMenu_SelectCard_Patch
         {
@@ -99,5 +152,7 @@ namespace SimpleSpawner
             }
 
         }
+
+
     }
 }
